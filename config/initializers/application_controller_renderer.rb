c8e1c070c8f1519db9d7e93f487606394b6abb26
nonoka57 <nonoka57nonoka@gmail.com>
2021-10-10 10:37:37 +0000

    [add]画像投稿

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4b900ea5d16340733862ea39d5d9f05d745237e0fa7e8d4e8f7d93fe3e742298367c14456dadefdf7d9c6a651a8247dcdc3c02b1e7bd373e2ae807f422175384'
